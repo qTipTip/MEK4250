6c1c305d89b706ed0b60afd8e3c6e0672441aee5
Ivar Stangeby <istangeby@gmail.com>
2017-05-01 20:21:59 +0200

    Exercise 7.6 done

--- assignments/assignment2/src/exercise_7_6.py
+++ assignments/assignment2/src/exercise_7_6.py
@@ -1,5 +1,6 @@
 from fenics import *
 from pandas import DataFrame
+import numpy as np
 
 set_log_active(False)
 
@@ -19,7 +20,7 @@ def solve_system(N, degree_V, degree_Q, file_dump=False):
     # Specific problem
     f = Expression(
         [
-            "pi * pi * sin(pi * x[1]) - 2 * pi * cos(x[0])",
+            "pi * pi * sin(pi * x[1]) - 2 * pi * cos(2*pi*x[0])",
             "pi * pi * cos(pi * x[0])"
         ],
         degree=degree_V)
@@ -50,27 +51,48 @@ def solve_system(N, degree_V, degree_Q, file_dump=False):
 
     if file_dump:
         fluid_file = File(
-            'fluid_{n}_{dv}_{dq}.pvd'.format(n=N, dv=degree_V, dq=degree_Q))
-        pressure_file = File('pressure_d{n}_{dv}_{dq}.pvd'.format(
+            'data/fluid_{n}_{dv}_{dq}.pvd'.format(n=N, dv=degree_V, dq=degree_Q))
+        pressure_file = File('data/pressure_d{n}_{dv}_{dq}.pvd'.format(
             n=N, dv=degree_V, dq=degree_Q))
         fluid_file << U
         pressure_file << P
 
     error_u = errornorm(u_analytical, U, 'H1', degree_rise=2)
     error_p = errornorm(p_analytical, P, 'L2', degree_rise=2)
+    error_sum = error_u + error_p
+    return error_u, error_p, error_sum
 
-    return error_u, error_p
 
+def estimate_error(error):
+    """
+    Finds the line best suiting the data and returns intersection and slope.
+    
+    :param error: pandas DataFrame
+    """
+    
+    N = error.index.values
+    parameters = error.columns.values
+    best_fit = DataFrame(
+        index=parameters, columns=['convergence rate', 'coefficient'])
+    h_log = [np.log(1.0 / n) for n in N]
+    error_log = error.applymap(np.log)
+    error_fit = np.polyfit(h_log, error_log['u + p'], deg=1)
+    # exponentiate to regain coefficients
+    error_fit[1] = np.exp(error_fit[1])
+    return list(error_fit)
 
 if __name__ == "__main__":
 
     element_degrees = [(4, 3), (4, 2), (3, 2), (3, 1)]
     N_values = [8, 16, 32, 64]
+    error_table = DataFrame(index=N_values, columns=['u + p'])
+    approximation_table = DataFrame(index=element_degrees, columns=['convergence rate', 'coefficient'])
     for element in element_degrees:
-        error_table = DataFrame(index=N_values, columns=['u', 'p'])
         V_deg, Q_deg = element
         for N in N_values:
             errors = solve_system(N, V_deg, Q_deg, file_dump=True)
-            error_table.set_value(N, 'u', errors[0])
-            error_table.set_value(N, 'p', errors[1])
-        print(error_table.to_latex())
+            error_table.set_value(N, 'u + p', errors[2])
+        best_fit_table = estimate_error(error_table)
+        approximation_table.set_value(element, 'convergence rate', best_fit_table[0])
+        approximation_table.set_value(element, 'coefficient', best_fit_table[1])
+    print(approximation_table.to_latex())
